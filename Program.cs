3c85f44bd1d6157576ed7c452a8d4702412446e5
RokiWielki <Cruck222@interia.pl>
2022-10-30 14:54:35 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Most
+{
+	public interface ITelewizor
+	{
+
+		int Kanal { get; set; }
+		//
+		//
+		void ZmienKanal(int kanal);
+
+	}
+
+
+
+	public class TvLg : ITelewizor
+	{
+
+		public TvLg()
+		{
+			this.Kanal = 1;
+		}
+
+		public int Kanal { get; set; }
+
+		public void Wlacz()
+		{
+			Console.WriteLine("Telewizor LG włączony.");
+		}
+
+		public void Wylacz()
+		{
+			//
+			//
+		}
+
+		public void ZmienKanal(int kanal)
+		{
+			//
+			//
+		}
+
+	}
+
+
+
+	public class TvXiaomi : ITelewizor
+	{
+
+		//
+		//
+
+	}
+
+
+
+	public abstract class PilotAbstrakcyjny
+	{
+
+		private ITelewizor tv;
+
+		public PilotAbstrakcyjny(ITelewizor tv)
+		{
+			//
+			//
+		}
+
+		//
+		//
+
+		public void ZmienKanal(int kanal)
+		{
+			//
+			//
+		}
+
+	}
+
+
+
+	public class PilotHarmony : PilotAbstrakcyjny
+	{
+
+		public PilotHarmony(ITelewizor tv) : base(tv) { }
+
+		public void DoWlacz()
+		{
+			Console.WriteLine("Pilot Harmony włącza telewizor...");
+			Wlacz();
+		}
+
+		//
+		//
+
+	}
+
+	public class PilotPhilips : PilotAbstrakcyjny
+	{
+
+		//
+		//
+
+		public void DoWylacz()
+		{
+			Console.WriteLine("Pilot Philips wyłącza telewizor...");
+			Wylacz();
+		}
+
+	}
+
+
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+			ITelewizor tv = new TvLg();
+			//
+			//
+
+			pilotHarmony.DoWlacz();
+			Console.WriteLine("Kanał: " + tv.Kanal);
+			pilotPhilips.DoZmienKanal(100);
+			Console.WriteLine("Kanał: " + tv.Kanal);
+			pilotHarmony.DoWylacz();
+
+		}
+	}
+}
